b196227bfe0ac18c5e4bddb2f70ecec11f8ebeaa
Maksym Shcherban <peregrin@ukr.net>
2022-11-26 14:08:45 +0100

    create Loader

---
+++ src/components/ui/Loader.jsx
@@ -0,0 +1,11 @@
+import './Loader.scss';
+import React from 'react';
+
+export default function Loader() {
+  return (
+    <div className="css-loader">
+      <div />
+      <div />
+    </div>
+  );
+}
